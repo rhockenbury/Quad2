59984c68fc25d16d7729e5d20e7a2b6be0f5da0b
Ryler Hockenbury <ryler.hockenbury@gmail.com>
2013-05-30 18:03:22 -0400

    inital PID commit

---
+++ Quad2/Main/Quad_FlightControl/PID.h
@@ -0,0 +1,16 @@
+/*
+ * PID.h
+ *
+ *  Created on: May 30, 2013
+ *      Author: ryler
+ */
+
+#ifndef PID_H_
+#define PID_H_
+
+// pass in current orientation
+// pass in stick commands
+// calculate motor commands
+
+
+#endif /* PID_H_ */
