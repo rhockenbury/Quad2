2d5447adcb43b98a5ad241863537c9ef01c66315
Ryler Hockenbury <ryler.hockenbury@gmail.com>
2013-05-30 18:03:38 -0400

    updated onGround global variables

--- Quad2/Libraries/Quad_Defines/globals.h
+++ Quad2/Libraries/Quad_Defines/globals.h
@@ -21,24 +21,24 @@
 #define ON 1
 #define OFF 0
 
-#define onGround 0
-#define inAir    1
-
 #define GYRO_READY   0x1
 #define ACCEL_READY  0x2
 #define MAG_READY    0x4
 #define BATT_READY   0x8
 #define RX_READY     0x16
 #define TX_READY     0x32
+#define MOTOR_READY  0x64
 
+extern bool onGround;
 extern uint8_t vehicleStatus;
 
-#define THROTTLE 1
-#define ROLL     2
-#define PITCH    3
-#define YAW      4
-#define AUX1     5
-#define AUX2     6
+// need to adjust these based on radio
+#define THROTTLE 0
+#define ROLL     1
+#define PITCH    2
+#define YAW      3
+#define AUX1     4
+#define AUX2     5
 
 #define MICROS  1000000  // 1 second = 1*10^6 microseconds
 #define MILLIS  1000     // 1 second = 1*10^3 milliseconds
