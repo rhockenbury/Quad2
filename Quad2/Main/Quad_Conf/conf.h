290d6994bbb8b67d89c8bd1f0852b94de8c05ecf
Ryler Hockenbury <ryler.hockenbury@gmail.com>
2013-08-21 14:39:16 -0400

    Added radio configs

--- Quad2/Main/Quad_Conf/conf.h
+++ Quad2/Main/Quad_Conf/conf.h
@@ -8,7 +8,17 @@
 #ifndef CONF_H_
 #define CONF_H_
 
+// PID parameters
 
+// debug parameters
 
+// serial chart or ground station
+// serial rate
+
+//
+
+#define RADIO_NUM_CHANNELS  6    // number of radio channels
+#define RADIO_SMOOTH_FACTOR 0.0  // adjust stick responsiveness
+#define RADIO_SCALE_FACTOR  1.0  // adjust stick range
 
 #endif /* CONF_H_ */
