61728c782daf0baecac136fd29b41bc8a56911f5
Ryler Hockenbury <ryler.hockenbury@gmail.com>
2013-08-22 20:57:48 -0400

    intial implementation

---
+++ Quad2/Libraries/Quad_Motors/motors.cpp
@@ -0,0 +1,75 @@
+/*
+ * motors.cpp
+ *
+ *  Created on: Aug 22, 2013
+ *      Author: Ryler Hockenbury
+ *
+ */
+
+#include "motors.h"
+#include <servo.h>
+
+Servo esc[4];
+
+
+/*
+ *
+ */
+void Motors::Motors() {
+
+}
+
+
+/*
+ *
+ */
+void Motors::init() {
+    esc[MOTOR1].attach(MOTOR1_PIN, MOTOR_COMMAND_MIN, MOTOR_COMMAND_MAX);
+    esc[MOTOR2].attach(MOTOR2_PIN, MOTOR_COMMAND_MIN, MOTOR_COMMAND_MAX);
+    esc[MOTOR3].attach(MOTOR3_PIN, MOTOR_COMMAND_MIN, MOTOR_COMMAND_MAX);
+    esc[MOTOR4].attach(MOTOR4_PIN, MOTOR_COMMAND_MIN, MOTOR_COMMAND_MAX);
+}
+
+
+/*
+ *
+ */
+void Motors::armMotors() {
+    commandAllMotors(MOTOR_COMMAND_MIN);
+    delay(200);
+}
+
+void Motors::disarmMotors() {
+
+}
+
+/*
+ *
+ */
+void Motors::pulseMotors(uint8_t numPulses) {
+    for(uint8_t pulseIndex; pulseIndex < numPulses; pulseIndex++) {
+    	 commandAllMotors(1100);
+    	 delay(200);
+    }
+}
+
+
+/*
+ *
+ */
+void Motors::commandMotor(uint8_t numMotor, float command) {
+    esc[numMotor].writeMicroseconds(command);
+}
+
+
+/*
+ *
+ */
+void Motors::commandAllMotors(float command) {
+	esc[MOTOR1].writeMicroseconds(command);
+	esc[MOTOR2].writeMicroseconds(command);
+	esc[MOTOR3].writeMicroseconds(command);
+	esc[MOTOR4].writeMicroseconds(command);
+}
+
+
--- Quad2/Libraries/Quad_Motors/motors.h
+++ Quad2/Libraries/Quad_Motors/motors.h
@@ -2,13 +2,39 @@
  * motors.h
  *
  *  Created on: May 30, 2013
- *      Author: ryler
+ *      Author: Ryler Hockenbury
  */
 
-#ifndef MOTORS_H_
-#define MOTORS_H_
+#ifndef motors_h
+#define motors_h
+
+#define MOTOR1 0
+#define MOTOR2 1
+#define MOTOR3 2
+#define MOTOR4 3
+
+#define MOTOR1_PIN 10
+#define MOTOR2_PIN 11
+#define MOTOR3_PIN 12
+#define MOTOR4_PIN 13
+
+#define MOTOR_COMMAND_MIN 1000
+#define MOTOR_COMMAND_MAX 2000
+
+class Motors {
+
+    public:
+	    Motors();
+	    void init();
+    	void armMotors();
+    	void disarmMotors();
+	    void pulseMotors(uint8_t numPulses);
+	    void commandMotor(uint8_t numMotor, float command);
+        void commandAllMotors(float command);
+
+    private:
 
 
+};
 
-
-#endif /* MOTORS_H_ */
+#endif /* motors_h */
