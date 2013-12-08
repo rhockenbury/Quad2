f43cc28a7d74e5a644be65ecab416cc51ebc94e9
Ryler Hockenbury <ryler.hockenbury@gmail.com>
2013-12-08 12:24:39 -0500

    Updated motor library

--- Quad2/Libraries/Quad_Motors/motors.cpp
+++ Quad2/Libraries/Quad_Motors/motors.cpp
@@ -13,7 +13,7 @@ Servo esc[4];
 
 
 /*
- *
+ * Empty
  */
 Motors::Motors() {
 
@@ -21,55 +21,74 @@ Motors::Motors() {
 
 
 /*
- *
+ * Bind ESC variables to output pins
  */
 void Motors::init() {
-    esc[MOTOR1].attach(MOTOR1_PIN, MOTOR_COMMAND_MIN, MOTOR_COMMAND_MAX);
-    esc[MOTOR2].attach(MOTOR2_PIN, MOTOR_COMMAND_MIN, MOTOR_COMMAND_MAX);
-    esc[MOTOR3].attach(MOTOR3_PIN, MOTOR_COMMAND_MIN, MOTOR_COMMAND_MAX);
-    esc[MOTOR4].attach(MOTOR4_PIN, MOTOR_COMMAND_MIN, MOTOR_COMMAND_MAX);
+	esc[MOTOR_FRONTLEFT].attach(MOTOR1_PIN, MOTOR_COMMAND_MIN, MOTOR_COMMAND_MAX);
+	esc[MOTOR_FRONTRIGHT].attach(MOTOR2_PIN, MOTOR_COMMAND_MIN, MOTOR_COMMAND_MAX);
+	esc[MOTOR_BACKLEFT].attach(MOTOR3_PIN, MOTOR_COMMAND_MIN, MOTOR_COMMAND_MAX);
+	esc[MOTOR_BACKRIGHT].attach(MOTOR4_PIN, MOTOR_COMMAND_MIN, MOTOR_COMMAND_MAX);
+
+	vehicleStatus = vehicleStatus | MOTOR_READY;
 }
 
 
 /*
- *
+ * Arm motors
  */
 void Motors::armMotors() {
     commandAllMotors(MOTOR_COMMAND_MIN);
     delay(200);
+
+    // delay long enough for start up tones, battery cell
+    // count tones, and long beep tones
 }
 
+/*
+ * Disarm motors
+ */
 void Motors::disarmMotors() {
+	commandAllMotors(MOTOR_COMMAND_MIN);
 
 }
 
 /*
- *
+ * Disconnect motors
+ */
+void Motors::killMotors() {
+	esc[MOTOR_FRONTLEFT].detach();
+	esc[MOTOR_FRONTRIGHT].detach();
+	esc[MOTOR_BACKLEFT].detach();
+	esc[MOTOR_BACKRIGHT].detach();
+}
+
+/*
+ * Pulse motors are low rotational speed
  */
 void Motors::pulseMotors(uint8_t numPulses) {
     for(uint8_t pulseIndex; pulseIndex < numPulses; pulseIndex++) {
-    	 commandAllMotors(1100);
+    	 commandAllMotors(MOTOR_COMMAND_MIN + 100);
     	 delay(200);
     }
 }
 
 
 /*
- *
+ * Command numMotor to input command
  */
-void Motors::commandMotor(uint8_t numMotor, float command) {
+void Motors::commandMotor(uint8_t numMotor, int command) {
     esc[numMotor].writeMicroseconds(command);
 }
 
 
 /*
- *
+ * Command all motors to input command
  */
-void Motors::commandAllMotors(float command) {
-	esc[MOTOR1].writeMicroseconds(command);
-	esc[MOTOR2].writeMicroseconds(command);
-	esc[MOTOR3].writeMicroseconds(command);
-	esc[MOTOR4].writeMicroseconds(command);
+void Motors::commandAllMotors(int command) {
+	esc[MOTOR_FRONTLEFT].writeMicroseconds(command);
+	esc[MOTOR_FRONTRIGHT].writeMicroseconds(command);
+	esc[MOTOR_BACKLEFT].writeMicroseconds(command);
+	esc[MOTOR_BACKRIGHT].writeMicroseconds(command);
 }
 
 
--- Quad2/Libraries/Quad_Motors/motors.h
+++ Quad2/Libraries/Quad_Motors/motors.h
@@ -8,31 +8,33 @@
 #ifndef motors_h
 #define motors_h
 
-#define MOTOR1 0
-#define MOTOR2 1
-#define MOTOR3 2
-#define MOTOR4 3
+#define MOTOR_FRONTLEFT  0
+#define MOTOR_FRONTRIGHT 1
+#define MOTOR_BACKLEFT   2
+#define MOTOR_BACKRIGHT  3
 
-#define MOTOR1_PIN 10
-#define MOTOR2_PIN 11
-#define MOTOR3_PIN 12
-#define MOTOR4_PIN 13
+#define MOTOR1_PIN 3
+#define MOTOR2_PIN 4
+#define MOTOR3_PIN 5
+#define MOTOR4_PIN 6
 
 #define MOTOR_COMMAND_MIN 1000
 #define MOTOR_COMMAND_MAX 2000
 
 #include <arduino.h>
+#include <../Quad_Defines/globals.h>
 
 class Motors {
 
     public:
 	    Motors();
-	    void init();
-    	void armMotors();
-    	void disarmMotors();
-	    void pulseMotors(uint8_t numPulses);
-	    void commandMotor(uint8_t numMotor, float command);
-        void commandAllMotors(float command);
+	    static void init();
+    	static void armMotors();
+    	static void disarmMotors();
+    	static void killMotors();
+	    static void pulseMotors(uint8_t numPulses);
+	    static void commandMotor(uint8_t numMotor, int command);
+        static void commandAllMotors(int command);
 
     private:
 
