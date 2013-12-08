a01b1d5b2647363bfdf883d93087efdf4d0bb686
Ryler Hockenbury <ryler.hockenbury@gmail.com>
2013-12-08 12:24:04 -0500

    Reformatted i2c code

--- Quad2/Libraries/Quad_I2C/I2Cdev.cpp
+++ Quad2/Libraries/Quad_I2C/I2Cdev.cpp
@@ -11,180 +11,181 @@
 
 #include "I2Cdev.h"
 
-I2Cdev::I2Cdev() { /* empty */ }
+uint16_t I2Cdev::readTimeout = 1000; // 1 sec
 
-uint16_t I2Cdev::readTimeout = 1000;
+I2Cdev::I2Cdev() {
+	/* empty */
+}
 
-//Method for writing a bit to an address on an I2C device
+/*
+ * Write a bit to an address on an I2C device
+ */
 bool I2Cdev::writeBit(uint8_t devAddr, uint8_t regAddr, uint8_t bitNum, 
-  uint8_t data)
-{
-  uint8_t b; 
-  readByte(devAddr, regAddr, &b); 
-  b = (data != 0) ? (b | (1 << bitNum)) : (b & ~(1 << bitNum));
-  return writeByte(devAddr, regAddr, b);
+		uint8_t data) {
+	uint8_t b;
+	readByte(devAddr, regAddr, &b);
+	b = (data != 0) ? (b | (1 << bitNum)) : (b & ~(1 << bitNum));
+	return writeByte(devAddr, regAddr, b);
 }
 
-
-
-//Method for writing bits to an address on an I2C device
+/*
+ * Write bits to an address on an I2C device
+ */
 bool I2Cdev::writeBits(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, 
-  uint8_t length, uint8_t data)
-{
-  uint8_t b;
+		uint8_t length, uint8_t data) {
+	uint8_t b;
     if (readByte(devAddr, regAddr, &b) != 0) {
-      uint8_t mask = ((1 << length) - 1) << (bitStart - length + 1);
-      data <<= (bitStart - length + 1);   // shift data into correct position
-      data &= mask;   // zero all non-important bits in data
-      b &= ~(mask);   // zero all important bits in existing byte
-      b |= data;      // combine data with existing byte
-      return writeByte(devAddr, regAddr, b);
+    	uint8_t mask = ((1 << length) - 1) << (bitStart - length + 1);
+    	data <<= (bitStart - length + 1);   // shift data into correct position
+    	data &= mask;   // zero all non-important bits in data
+    	b &= ~(mask);   // zero all important bits in existing byte
+    	b |= data;      // combine data with existing byte
+    	return writeByte(devAddr, regAddr, b);
     } 
-    else 
-      return false;
-
+    else {
+    	return false;
+    }
 }
 
-
-
-//Method for writing a byte to an address on an I2C device
-bool I2Cdev::writeByte(uint8_t devAddr, uint8_t regAddr, uint8_t data) 
-{
-  return writeBytes(devAddr, regAddr, 1, &data);  
+/*
+ * Write a byte to an address on an I2C device
+ */
+bool I2Cdev::writeByte(uint8_t devAddr, uint8_t regAddr, uint8_t data) {
+	return writeBytes(devAddr, regAddr, 1, &data);
 }
 
-
-
-//Method for writing bytes to an address on an I2C device
+/*
+ * Write bytes to an address on an I2C device
+ *
+ * Error Codes:
+ *  0: Success
+ *  1: Data too long to fit in transmit buffer
+ *  2: Received NACK on transmit of address
+ *  3: Received NACK on transmit of data
+ *  4: Other error
+ */
 bool I2Cdev::writeBytes(uint8_t devAddr, uint8_t regAddr, uint8_t length, 
-   uint8_t *data)
-{
-  #ifdef I2CDEV_SERIAL_DEBUG
-    Serial.print("I2C (0x"); 
-    Serial.print(devAddr, HEX); 
-    Serial.print(") writing "); 
-    Serial.print(length, DEC); 
-    Serial.print(" byte(s)to 0x"); 
-    Serial.print(regAddr, HEX); 
-    Serial.print("..."); 
-  #endif
+		uint8_t *data) {
+	uint8_t status = 0;
 
-  uint8_t status = 0;
-
-  Wire.beginTransmission(devAddr); 
-  Wire.write(regAddr); 
-
-  uint8_t i;
-  for(i = 0; i < length; i++)
-  { 
-  	Wire.write( (uint8_t) data[i]);
-
-    #ifdef I2CDEV_SERIAL_DEBUG
-    Serial.print(data[i], HEX);
-    if (i + 1 < length) Serial.print("  ");
+	#ifdef I2CDEV_SERIAL_DEBUG
+    	Serial.print("I2C (0x");
+    	Serial.print(devAddr, HEX);
+    	Serial.print(") writing ");
+    	Serial.print(length, DEC);
+    	Serial.print(" byte(s)to 0x");
+    	Serial.print(regAddr, HEX);
+    	Serial.print("...");
     #endif
-  }
 
-  status = Wire.endTransmission();
+    Wire.beginTransmission(devAddr);
+    Wire.write(regAddr);
 
-  #ifdef I2CDEV_SERIAL_DEBUG
-       Serial.println(". Done.");
-  #endif
+    for(uint8_t i = 0; i < length; i++) {
+    	Wire.write( (uint8_t) data[i]);
 
-  return (status == 0);
+		#ifdef I2CDEV_SERIAL_DEBUG
+    		Serial.print(data[i], HEX);
+    		if (i + 1 < length) Serial.print("  ");
+    	#endif
+    }
+
+    status = Wire.endTransmission();
+    errorMsg = status;
+
+  	#ifdef I2CDEV_SERIAL_DEBUG
+    	Serial.println(". Done.");
+  	#endif
+
+    return (status == 0);
 }
 
-
-
-//Method for reading a bit from an address on an I2C device
+/*
+ * Read a bit from an address on an I2C device
+ */
 int8_t I2Cdev::readBit(uint8_t devAddr, uint8_t regAddr, uint8_t bitNum, 
-    uint8_t *data, uint16_t TIMEOUT)
-{
-  uint8_t b;
-  uint8_t count = readByte(devAddr, regAddr, &b, TIMEOUT); 
+		uint8_t *data, uint16_t TIMEOUT) {
+	uint8_t b;
+	uint8_t count = readByte(devAddr, regAddr, &b, TIMEOUT);
 
-  *data = b & (0x1 << bitNum); 
-  return count; 
+	*data = b & (0x1 << bitNum);
+	return count;
 }
 
-
-
-//Method for reading bits from an address on an I2C device
+/*
+ * Read bits from an address on an I2C device
+ */
 int8_t I2Cdev::readBits(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, 
-   uint8_t length, uint8_t *data, uint16_t timeout) 
-{
-  uint8_t count, b;
-  if ((count = readByte(devAddr, regAddr, &b, timeout)) != 0) 
-  {
-    uint8_t mask = ((1 << length) - 1) << (bitStart - length + 1);
-    b &= mask;
-    b >>= (bitStart - length + 1);
-    *data = b;
-  }
+   uint8_t length, uint8_t *data, uint16_t timeout)  {
+	uint8_t count, b;
+	if ((count = readByte(devAddr, regAddr, &b, timeout)) != 0) {
+		uint8_t mask = ((1 << length) - 1) << (bitStart - length + 1);
+    	b &= mask;
+    	b >>= (bitStart - length + 1);
+    	*data = b;
+	}
 
-  return count;
+	return count;
 }
 
-
-
-//Method for reading a byte from an address on an I2C device
+/*
+ * Read a byte from an address on an I2C device
+ */
 int8_t I2Cdev::readByte(uint8_t devAddr, uint8_t regAddr, uint8_t *data, 
-  uint16_t TIMEOUT)
-{
-  return readBytes(devAddr, regAddr, 1, data, TIMEOUT); 
+  uint16_t TIMEOUT) {
+	return readBytes(devAddr, regAddr, 1, data, TIMEOUT);
 }
 
-
-
-//Method for reading bytes from an address on an I2C device
+/*
+ * Read bytes from an address on an I2C device
+ */
 int8_t I2Cdev::readBytes(uint8_t devAddr, uint8_t regAddr, uint8_t length, 
-  uint8_t *data, uint16_t TIMEOUT) 
-{
-  #ifdef I2CDEV_SERIAL_DEBUG
-    Serial.print("I2C (0x"); 
-    Serial.print(devAddr, HEX); 
-    Serial.print(") reading "); 
-    Serial.print(length, DEC); 
-    Serial.print(" byte(s) from 0x"); 
-    Serial.print(regAddr, HEX); 
-    Serial.print("..."); 
-  #endif
-
-  uint32_t tStart = millis(); 
-  uint8_t count = 0; // byte transfer counter 
-
-  Wire.beginTransmission(devAddr);
-  Wire.write(regAddr);
-  Wire.endTransmission();
-
-  Wire.beginTransmission(devAddr); 
-  Wire.requestFrom(devAddr, length);
-
-  while( Wire.available() && (TIMEOUT == 0 || (millis() - tStart) < TIMEOUT) )
-  { 
-    data[count] = Wire.read(); 
+  uint8_t *data, uint16_t TIMEOUT) {
 
     #ifdef I2CDEV_SERIAL_DEBUG
-      Serial.print(data[count], HEX);
-      if (count + 1 < length) Serial.print(" ");
-    #endif
+    	Serial.print("I2C (0x");
+    	Serial.print(devAddr, HEX);
+    	Serial.print(") reading ");
+    	Serial.print(length, DEC);
+    	Serial.print(" byte(s) from 0x");
+    	Serial.print(regAddr, HEX);
+    	Serial.print("...");
+  	#endif
 
-    count++;
-  }
+    uint32_t tStart = millis();
+    uint8_t count = 0; // byte transfer counter
 
-  Wire.endTransmission(); 
+    Wire.beginTransmission(devAddr);
+    Wire.write(regAddr);
+    Wire.endTransmission();
 
-  //check for timeout
-  if( TIMEOUT > 0 && (millis() - tStart) >=TIMEOUT && count < length )
-    count = -1; 
+    Wire.beginTransmission(devAddr);
+    Wire.requestFrom(devAddr, length);
 
-  #ifdef I2CDEV_SERIAL_DEBUG
-    Serial.print(". Done ("); 
-    Serial.print(count, DEC); 
-    Serial.print(" read)."); 
-  #endif
+    while( Wire.available() && (TIMEOUT == 0 || (millis() - tStart) < TIMEOUT) ) {
+    	data[count] = Wire.read();
 
-  return count; 
+    	#ifdef I2CDEV_SERIAL_DEBUG
+    		Serial.print(data[count], HEX);
+    		if (count + 1 < length) Serial.print(" ");
+    	#endif
+
+    	count++;
+    }
+
+    Wire.endTransmission();
+
+    //check for timeout
+    if( TIMEOUT > 0 && (millis() - tStart) >=TIMEOUT && count < length )
+    	count = -1;
+
+	#ifdef I2CDEV_SERIAL_DEBUG
+    	Serial.print(". Done (");
+    	Serial.print(count, DEC);
+    	Serial.print(" read).");
+  	#endif
+
+    return count;
 }
 
 
--- Quad2/Libraries/Quad_I2C/I2Cdev.h
+++ Quad2/Libraries/Quad_I2C/I2Cdev.h
@@ -12,43 +12,35 @@
 #ifndef I2Cdev_h
 #define I2Cdev_h
 
-#include <arduino.h>   // for millis()
+#include <arduino.h>
 #include <inttypes.h>
 #include <Wire.h>
-        
-//#define I2CDEV_SERIAL_DEBUG - move to conf
+#include <../Main/Quad_Conf/conf.h>
 
-class I2Cdev
-{
+class I2Cdev {
   public:
     I2Cdev(); 
 
+    static bool writeBit(uint8_t devAddr, uint8_t regAddr,
+    		uint8_t bitNum, uint8_t data);
+    static bool writeBits(uint8_t devAddr, uint8_t regAddr,
+    		uint8_t bitStart, uint8_t length, uint8_t data);
+    static bool writeByte(uint8_t devAddr, uint8_t regAddr,
+    		uint8_t data);
+    static bool writeBytes(uint8_t devAddr, uint8_t regAddr,
+    		uint8_t length, uint8_t *data);
     
-    static bool writeBit(uint8_t devAddr, uint8_t regAddr, uint8_t bitNum, 
-      uint8_t data);
-
-    static bool writeBits(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, 
-      uint8_t length, uint8_t data); 
-
-    static bool writeByte(uint8_t devAddr, uint8_t regAddr, uint8_t data); 
-
-    static bool writeBytes(uint8_t devAddr, uint8_t regAddr, uint8_t length, 
-      uint8_t *data);
-    
-
     static int8_t readBit(uint8_t devAddr, uint8_t regAddr, uint8_t bitNum, 
-      uint8_t *data, uint16_t TIMEOUT=I2Cdev::readTimeout);
-
+    	uint8_t *data, uint16_t TIMEOUT=I2Cdev::readTimeout);
     static int8_t readBits(uint8_t devAddr, uint8_t regAddr, uint8_t bitStart, 
-      uint8_t length, uint8_t *data, uint16_t TIMEOUT=I2Cdev::readTimeout); 
-
+    	uint8_t length, uint8_t *data, uint16_t TIMEOUT=I2Cdev::readTimeout);
     static int8_t readByte(uint8_t devAddr, uint8_t regAddr, uint8_t *data, 
-      uint16_t TIMEOUT=I2Cdev::readTimeout); 
-
+    	uint16_t TIMEOUT=I2Cdev::readTimeout);
     static int8_t readBytes(uint8_t devAddr, uint8_t regAddr, uint8_t length, 
       uint8_t *data, uint16_t TIMEOUT=I2Cdev::readTimeout);
 
     static uint16_t readTimeout; 
+    static uint8_t errorMsg;
     
 };
 
