5f4a3d8b93e47751acc8b16582f739ba8877b9bf
Ryler Hockenbury <ryler.hockenbury@gmail.com>
2013-12-08 12:26:31 -0500

    Changed smooth factor to array

--- Quad2/Main/Quad_Conf/conf.h
+++ Quad2/Main/Quad_Conf/conf.h
@@ -17,8 +17,15 @@
 
 //
 
+//#define I2CDEV_SERIAL_DEBUG
+
 #define RADIO_NUM_CHANNELS  6     // number of radio channels
-#define RADIO_SMOOTH_FACTOR 0.9  // adjust stick responsiveness
-#define RADIO_SCALE_FACTOR  1.0  // adjust stick range
+//#define RADIO_SMOOTH_FACTOR {0.9, 0.9, 0.9, 0.9, 0.9, 0.9}   // adjust stick responsiveness
+//#define RADIO_SCALE_FACTOR  1.0   // adjust stick range (0.5 will double range, 2 will half range)
+#define MAX_COMMAND_ANGLE 45.0  //
+
+
+const float RADIO_SMOOTH_FACTOR[RADIO_NUM_CHANNELS] = {0.9, 0.9, 0.9, 0.9, 0.9, 0.9};
+const float RADIO_SCALE_FACTOR[RADIO_NUM_CHANNELS] = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
 
 #endif /* CONF_H_ */
